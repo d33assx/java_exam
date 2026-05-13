4bf4777eb85424aea3789db09dfc92da689f3f6b
Dias Abdullayev <d33assx@gmail.com>
2026-05-13 17:55:19 +0500

    async config

---
+++ src/main/java/com/exam/dias/config/AbdullayevDiasAsyncConfig.java
@@ -0,0 +1,9 @@
+package com.exam.dias.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AbdullayevDiasAsyncConfig {
+}
