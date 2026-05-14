ac43b4b60487b33129809be6f38ac2dc8ceb43ca
Dias Abdullayev <d33assx@gmail.com>
2026-05-14 10:12:05 +0500

    swagger docs

---
+++ src/main/java/com/exam/dias/config/AbdullayevDiasSwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.exam.dias.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AbdullayevDiasSwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Online Course Platform API")
+                        .version("1.0")
+                        .description("LMS API Documentation - Dias Abdullayev"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)));
+    }
+}
